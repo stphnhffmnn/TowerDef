18858631000bcd0ea541e15bd8e250a67b5ac6da
stphnhffmnn <stphnhffmnn@gmail.com>
2016-06-15 19:45:16 -0700

    updates

--- AndriodStarter/Assets/QuestMapController.cs
+++ AndriodStarter/Assets/QuestMapController.cs
@@ -4,9 +4,13 @@ using System.Collections;
 
 public class QuestMapController : MonoBehaviour {
     public Button[] QuestButtons = new Button[10];
+    public Button levelMenu;
+    public Transform LevelUpMenu;
+    public bool levelUp;
 	// Use this for initialization
     public int QuestsComplete = 0;
 	void Start () {
+        levelUp = false;
         QuestsComplete = PlayerPrefs.GetInt("QuestLevels");
 	}
 	
@@ -16,9 +20,32 @@ public class QuestMapController : MonoBehaviour {
         {
             QuestButtons[i].GetComponent<QuestButtonClass>().isActive = true;
         }
-	}
+        if (levelUp)//EcoMenu
+        {
+            LevelUpMenu.GetComponent<Animator>().Play("EcoMenuAnim");
+            LevelUpMenu.GetComponent<Animator>().speed = 2;
+           
+
+        }
+        if (!levelUp)
+        {
+            LevelUpMenu.GetComponent<Animator>().Play("EcoMenuClose");
+            LevelUpMenu.GetComponent<Animator>().speed = 2;
+         
+        }
+
+    }
     public void MenuMain()
     {
         Application.LoadLevel("MainMenu");
     }
+    public void LevelUpMenuChange()
+    {
+        if(levelUp)
+        {
+            levelUp = false;
+        }
+        else { levelUp = true; }
+    }
+
 }
